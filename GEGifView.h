f770d94ee58d8a552f9446b7fceaa13a09d5d420
sunyanliang <yanliang.sun@smellme.cn>
2013-09-14 17:41:14 +0800

    GEGifView1.0发布了：）

---
+++ GEGifView.h
@@ -0,0 +1,59 @@
+//
+//  GEGifView.h
+//
+//  Created by godera on 9/14/13.
+//  Copyright (c) 2013. All rights reserved.
+//
+//  QQ: 719181178
+//from SvGifView and OLImageView
+//MRC MRC MRC MRC MRC MRC MRC MRC MRC MRC MRC
+
+//本GIF类打印语句的开关信息，设为0即关
+#ifdef DEBUG
+#define DEBUG_SWITCH_GIF 0
+#else
+#define DEBUG_SWITCH_GIF 0
+#endif
+
+#if DEBUG_SWITCH_GIF
+#define GELOG_GIF NSLog
+#else
+#define GELOG_GIF(...)
+#endif
+
+/*usage:
+ GEGifView* gifView = [[[GEGifView alloc] initWithFrame:CGRectMake(0, 100, 320, 130)] autorelease];
+ gifView.fileName = @"jiafei.gif";
+ gifView.repeatCount = 2;
+ [self.view addSubview:gifView];
+ _gifView = gifView;
+ [gifView start];
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface GEGifView : UIView
+
+//data source properties
+@property (nonatomic, copy) NSData* data;//gif data
+@property (nonatomic, copy) NSString* fileName;//gif file from bundle
+@property (nonatomic, copy) NSString* filePath;//gif file path
+
+@property (nonatomic, assign) NSInteger repeatCount;//defaults to infinite
+
+/* brief The default mode (NSDefaultRunLoopMode), causes the animation to pauses while it is contained in an actively scrolling `UIScrollView`. 
+         Use NSRunLoopCommonModes if you don't want this behavior.
+ */
+@property (nonatomic, copy) NSString *runLoopMode;
+
+@property (nonatomic, assign) BOOL clearWhenStop;//defaults to YES.
+
+- (id)initWithFrame:(CGRect)frame;//default init method
+
+- (void)start;
+
+- (void)stop;
+
+- (void)pause;
+
+@end
---
+++ GEGifView.m
@@ -0,0 +1,236 @@
+//
+//  GEGifView.m
+//
+//  Created by godera on 3/28/13.
+//  Copyright (c) 2013. All rights reserved.
+//
+
+#import "GEGifView.h"
+#import <ImageIO/ImageIO.h>
+#import <QuartzCore/CoreAnimation.h>
+
+@interface GEGifView() {
+    CADisplayLink* _displayLink;
+    NSRunLoop* _runloop;
+    
+    NSMutableArray* _frameImages;//CGImageRefs
+    NSMutableArray* _frameDelayTimes;
+    NSMutableArray* _frameStartTimes;//the 0 frame corresponds to time point 0.
+    NSInteger _comparedFrameIndex;
+    NSTimeInterval _currentTimePoint;//compared to item in _frameStartTimes
+    
+    CGFloat _totalTime;         // seconds
+    CGFloat _width;
+    CGFloat _height;
+    
+    NSInteger _decreasingCount;
+}
+
+@end
+
+
+@implementation GEGifView
+
+- (void)dealloc
+{
+    [_data release];
+    [_filePath release];
+    [_fileName release];
+
+    [_runLoopMode release];
+    
+    [_displayLink release];
+    [_runloop release];
+    
+    [_frameImages release];
+    [_frameDelayTimes release];
+    [_frameStartTimes release];
+    
+    [super dealloc];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.contentMode = UIViewContentModeScaleAspectFit;
+        
+        _runLoopMode = NSDefaultRunLoopMode;
+        _runloop = [NSRunLoop mainRunLoop];
+        
+        _clearWhenStop = YES;
+        
+        _frameImages = [NSMutableArray new];
+        _frameDelayTimes = [NSMutableArray new];
+        _frameStartTimes = [NSMutableArray new];
+        _comparedFrameIndex = 0;
+        
+        _totalTime = 0;
+        _width = 0;
+        _height = 0;
+        
+        _repeatCount = NSUIntegerMax;
+    }
+    return self;
+}
+
+
+-(void)setData:(NSData *)data
+{
+    NSData* temp = [data copy];
+    [_data release];
+    _data = temp;
+    
+    CGImageSourceRef gifSource = CGImageSourceCreateWithData((CFDataRef)_data, NULL);
+    
+    [self getFrameInfosFromGifSource:gifSource];
+    
+    if (gifSource) {
+        CFRelease(gifSource);
+    }
+}
+
+-(void)setFileName:(NSString *)fileName
+{
+    NSString* temp = [fileName copy];
+    [_fileName release];
+    _fileName = temp;
+    
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:_fileName ofType:nil];
+    NSURL* fileURL = [NSURL fileURLWithPath:filePath];
+    CGImageSourceRef gifSource = CGImageSourceCreateWithURL((CFURLRef)fileURL, NULL);
+    
+    [self getFrameInfosFromGifSource:gifSource];
+    
+    if (gifSource) {
+        CFRelease(gifSource);
+    }
+}
+
+-(void)setFilePath:(NSString *)filePath
+{
+    NSString* temp = [filePath copy];
+    [_filePath release];
+    _filePath = temp;
+    
+    NSURL* fileURL = [NSURL fileURLWithPath:filePath];
+    CGImageSourceRef gifSource = CGImageSourceCreateWithURL((CFURLRef)fileURL, NULL);
+    
+    [self getFrameInfosFromGifSource:gifSource];
+    
+    if (gifSource) {
+        CFRelease(gifSource);
+    }
+}
+
+
+/*
+ * @brief gets gif information
+ */
+- (void)getFrameInfosFromGifSource:(CGImageSourceRef)gifSource
+{
+    // init
+    _totalTime = 0;
+    [_frameImages removeAllObjects];
+    [_frameDelayTimes removeAllObjects];
+    [_frameStartTimes removeAllObjects];
+    
+    // get frame count
+    size_t frameCount = CGImageSourceGetCount(gifSource);
+    for (size_t i = 0; i < frameCount; ++i) {
+        // get each frame
+        CGImageRef frame = CGImageSourceCreateImageAtIndex(gifSource, i, NULL);
+        [_frameImages addObject:(id)frame];
+        CGImageRelease(frame);
+        
+        // get gif info with each frame
+        NSDictionary *dict = (NSDictionary*)CGImageSourceCopyPropertiesAtIndex(gifSource, i, NULL);
+        GELOG_GIF(@"kCGImagePropertyGIFDictionary %ld = %@", i,[dict objectForKey:(NSString*)kCGImagePropertyGIFDictionary]);
+        
+        // get gif size
+        _width = [[dict objectForKey:(NSString*)kCGImagePropertyPixelWidth] floatValue];
+        _height = [[dict objectForKey:(NSString*)kCGImagePropertyPixelHeight] floatValue];
+        
+        // kCGImagePropertyGIFDictionary中kCGImagePropertyGIFDelayTime，kCGImagePropertyGIFUnclampedDelayTime值是一样的
+        NSDictionary *gifDict = [dict objectForKey:(NSString*)kCGImagePropertyGIFDictionary];
+        
+        id aDelayTime = [gifDict objectForKey:(NSString*)kCGImagePropertyGIFDelayTime];
+        [_frameDelayTimes addObject:aDelayTime];
+        
+        _totalTime += [aDelayTime floatValue];
+
+        CFRelease(dict);
+    }
+    
+    // get frame start times
+    [_frameStartTimes addObject:@(0)];
+    CGFloat currentFrameStartTime = 0;
+    for (id aDelayTime in _frameDelayTimes) {
+        currentFrameStartTime += [aDelayTime floatValue];
+        [_frameStartTimes addObject:@(currentFrameStartTime)];
+    }
+
+}
+
+- (void)changeFrame:(CADisplayLink*)displayLink
+{
+    GELOG_GIF(@"compared time point = %f",[_frameStartTimes[_comparedFrameIndex] doubleValue]);
+    GELOG_GIF(@"current time point = %f",displayLink.duration);
+    
+    _currentTimePoint += displayLink.duration;
+    if (_currentTimePoint >= [_frameStartTimes[_comparedFrameIndex] doubleValue]) {
+        if (_comparedFrameIndex >= _frameImages.count) {//one loop
+            if (_repeatCount != NSUIntegerMax) {
+                _decreasingCount --;
+                if (_decreasingCount == 0) {
+                    [self stop];
+                    return;
+                }
+            }
+            _comparedFrameIndex = 0;
+            _currentTimePoint = 0;
+        }
+        self.layer.contents = _frameImages[_comparedFrameIndex];
+        _comparedFrameIndex ++;//next compared frame index
+    }
+}
+
+- (void)start
+{
+    if (_displayLink.paused == YES) {// recover from pause state
+        _displayLink.paused = NO;
+    }else{// a new start
+        _currentTimePoint = 0;
+        _comparedFrameIndex = 0;
+        _decreasingCount = _repeatCount;
+        
+        [_displayLink invalidate];
+        _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(changeFrame:)];
+        [_displayLink addToRunLoop:_runloop forMode:_runLoopMode];
+    }
+}
+
+- (void)stop
+{
+    [_displayLink invalidate];
+    _displayLink = nil;
+    
+    if (_clearWhenStop) {
+        self.layer.contents = nil;
+    }
+}
+
+- (void)pause
+{
+    _displayLink.paused = YES;
+}
+
+
+-(NSString *)description
+{
+    return [NSString stringWithFormat:@"fileName = %@, filePath = %@, repeatCount = %d, frameCount = %d",_fileName,_filePath,_repeatCount,_frameImages.count];
+}
+
+@end
+
+
